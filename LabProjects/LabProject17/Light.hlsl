728cf402dc90a46e8aceb95f82f7ea8532fc7921
Choi Hae Seong <39898936+chs0218@users.noreply.github.com>
2022-06-13 04:59:16 +0900

    Revert "Revert "tmp""
    This reverts commit 20911ab7d4693d63f71bedc3105e08574025f617.

--- LabProjects/LabProject17/Light.hlsl
+++ LabProjects/LabProject17/Light.hlsl
@@ -7,8 +7,6 @@
 #define _WITH_THETA_PHI_CONES
 //#define _WITH_REFLECT
 
-int gnMaterial = 0;
-float3 gvCameraPosition = { 0.0f, 0.0f, 0.0f };
 
 struct MATERIAL
 {
@@ -46,7 +44,6 @@ cbuffer cbLights : register(b4)
 
 float4 DirectionalLight(int nIndex, float3 vNormal, float3 vToCamera)
 {
-
 	float3 vToLight = -gLights[nIndex].m_vDirection;
 	float fDiffuseFactor = dot(vToLight, vNormal);
 	float fSpecularFactor = 0.0f;
@@ -56,19 +53,22 @@ float4 DirectionalLight(int nIndex, float3 vNormal, float3 vToCamera)
 		{
 #ifdef _WITH_REFLECT
 			float3 vReflect = reflect(-vToLight, vNormal);
-			fSpecularFactor = pow(max(dot(vReflect, vToCamera), 0.0f), gMaterials[gnMaterial].m_cSpecular.a);
+			fSpecularFactor = pow(max(dot(vReflect, vToCamera), 0.0f),
+				gMaterials[gnMaterial].m_cSpecular.a);
 #else
 #ifdef _WITH_LOCAL_VIEWER_HIGHLIGHTING
 			float3 vHalf = normalize(vToCamera + vToLight);
 #else
 			float3 vHalf = float3(0.0f, 1.0f, 0.0f);
 #endif
-			fSpecularFactor = pow(max(dot(vHalf, vNormal), 0.0f), gMaterials[gnMaterial].m_cSpecular.a);
+			fSpecularFactor = pow(max(dot(vHalf, vNormal), 0.0f),
+				gMaterials[gnMaterial].m_cSpecular.a);
 #endif
 		}
 	}
-
-	return((gLights[nIndex].m_cAmbient * gMaterials[gnMaterial].m_cAmbient) + (gLights[nIndex].m_cDiffuse * fDiffuseFactor * gMaterials[gnMaterial].m_cDiffuse) + (gLights[nIndex].m_cSpecular * fSpecularFactor * gMaterials[gnMaterial].m_cSpecular));
+	return((gLights[nIndex].m_cAmbient * gMaterials[gnMaterial].m_cAmbient) +
+		(gLights[nIndex].m_cDiffuse * fDiffuseFactor * gMaterials[gnMaterial].m_cDiffuse) +
+		(gLights[nIndex].m_cSpecular * fSpecularFactor * gMaterials[gnMaterial].m_cSpecular));
 }
 
 float4 PointLight(int nIndex, float3 vPosition, float3 vNormal, float3 vToCamera)
